fa749b7bbeeb60e3f5c2121e45c64f94d6f6ecd9
Emile Calixte <calixte.emile@gmail.com>
2022-05-20 14:53:13 +0200

    Transaction sender can transfer balance only if he is contract owner

--- 1-challenges-solidity-mes/4-5-6/deposit.sol
+++ 1-challenges-solidity-mes/4-5-6/deposit.sol
@@ -3,6 +3,12 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Deposit {
+    address private immutable admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
     receive() external payable {}
 
     function getBalance() public view returns(uint) {
@@ -10,6 +16,8 @@ contract Deposit {
     }
 
     function transferBalance(address payable to) public {
+        require(msg.sender == admin, "You must be the contract admin to transfer balance");
+
         uint amount = address(this).balance; // We can also use directly the getBalance() global function
         to.transfer(amount);
     }
