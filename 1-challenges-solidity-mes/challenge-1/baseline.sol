bdf033a67d91d29216e62ec762a96042cbc2c306
Emile Calixte <calixte.emile@gmail.com>
2022-05-20 12:22:55 +0200

    Register owner address in state variable

--- 1-challenges-solidity-mes/challenge-1/baseline.sol
+++ 1-challenges-solidity-mes/challenge-1/baseline.sol
@@ -4,7 +4,13 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract CryptosToken {
     string public constant name = "Cryptos";
+    address public owner;
     uint supply;
+
+    constructor() {
+        owner = msg.sender;
+        supply = 0;
+    }
 
     function getSupply() public view returns(uint) {
         return supply;
