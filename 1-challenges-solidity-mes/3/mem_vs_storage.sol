c3bd61da6cf24101ca1e7df137fbb77d76c9c8dc
Emile Calixte <calixte.emile@gmail.com>
2022-05-20 12:30:45 +0200

    Change function changeTokens to modify tokens state variable

--- 1-challenges-solidity-mes/3/mem_vs_storage.sol
+++ 1-challenges-solidity-mes/3/mem_vs_storage.sol
@@ -5,8 +5,8 @@ pragma solidity >=0.5.0 <0.9.0;
 contract MyTokens {
     string[] public tokens = ['BTC', 'ETH'];
     
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = 'VET';
     }
 }
