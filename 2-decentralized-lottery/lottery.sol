9f57cc9ae7e4c637eeb6b5875de42e6394592e4e
Emile Calixte <calixte.emile@gmail.com>
2022-05-23 10:18:23 +0200

    create functions to receive player participation and get contract balance

--- 2-decentralized-lottery/lottery.sol
+++ 2-decentralized-lottery/lottery.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
  
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract Lottery {
     address payable[] public players; // Anyone will be able to see the registered players
@@ -9,5 +9,18 @@ contract Lottery {
 
     constructor() {
         manager = msg.sender;
+    }
+
+    receive() external payable {
+        require(msg.value == 0.1 ether);
+
+        // Convert plain address to payable address
+        players.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint) {
+        require(msg.sender == manager);
+
+        return address(this).balance;
     }
 }
