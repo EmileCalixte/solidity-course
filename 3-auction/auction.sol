ef3476bfc7a11dc198cb3a1a595ad4d8b3bddf8b
Emile Calixte <calixte.emile@gmail.com>
2022-05-24 11:44:28 +0200

    Add function to place bid

--- 3-auction/auction.sol
+++ 3-auction/auction.sol
@@ -40,7 +40,52 @@ contract Auction {
         _; // <- Here comes the original function code
     }
 
+    modifier notOwner() {
+        require(owner != msg.sender);
+        _;
+    }
+
+    modifier afterStart() {
+        require(block.number >= startBlockNumber);
+        _;
+    }
+
+    modifier beforeEnd() {
+        require(block.number < endBlockNumber);
+        _;
+    }
+
     function changeOwner(address payable _owner) public onlyOwner {
         owner = _owner;
+    }
+
+    function cancelAuction() public onlyOwner {
+        state = AuctionState.Cancelled;
+    }
+
+    function placeBid() public payable notOwner afterStart beforeEnd {
+        require(state == AuctionState.Running, "The auction is not running");
+        require(msg.value >= 100); // For our tests, 100 wei is fine. In real cases, we should put a greater value, like 0.01 Ether for example
+
+        uint currentBid = bids[msg.sender] + msg.value; // The value already sent by the current address + the value sent with this transaction
+
+        require(currentBid > highestBindingBid, "Your bid must be higher than the current highest bid");
+
+        bids[msg.sender] = currentBid;
+
+        if (currentBid <= bids[highestBidder]) {
+            highestBindingBid = min(currentBid + bidIncrement, bids[highestBidder]);
+        } else {
+            highestBindingBid = min(currentBid, bids[highestBidder] + bidIncrement);
+            highestBidder = payable(msg.sender);
+        }
+    }
+
+    function min(uint a, uint b) internal pure returns(uint) {
+        if (a <= b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 }
