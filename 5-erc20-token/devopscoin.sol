7cbf8ce3a1937c84f8916c4cf1c7e766e8a82e1b
Emile Calixte <calixte.emile@gmail.com>
2022-05-25 11:52:52 +0200

    Add comment

--- 5-erc20-token/devopscoin.sol
+++ 5-erc20-token/devopscoin.sol
@@ -2,6 +2,10 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
+// ----------------------------------------------------------------------------
+// EIP-20: ERC-20 Token Standard
+// https://eips.ethereum.org/EIPS/eip-20
+// -----------------------------------------
 interface ERC20Token {
     function totalSupply() external view returns (uint);
 
